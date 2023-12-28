d0eedc9d3835f4fdccde06265286437a1e432d07
Edwin Amaranto Villegas <63481204+EdwinCoder85@users.noreply.github.com>
2023-12-28 00:40:45 -0500

    first commit

--- src/components/WarningUser.jsx
+++ src/components/WarningUser.jsx
@@ -6,27 +6,59 @@ const WarningUser = ({
   handleDelete,
   closeWarning,
   handleCloseWarning,
-  flag
+  flag,
 }) => {
-
   return (
-    <div onClick={handleCloseWarning} className={`warninguser-container ${closeWarning && 'close-warning'}`}>
-          <form onClick={e => e.stopPropagation()} className="warninguser">
-          <i className="fa-solid fa-circle-question warninguser__icon"></i>
-            <h2 className="warninguser__question">{flag === 'N' ? "Are you sure to create a user?" : flag === 'U' ? "Are you sure to update the user?" : "Are you sure to delete the user" }</h2>
-            <div onClick={handleCloseWarning} className="warninguser__close">x</div>
-            {/* <span className="warninguser__message">
-              El usuario <strong>{`${user?.first_name} ${user?.last_name}`}</strong> se ha {flag === 'N' ? "creado" : flag === 'U' ? "actualizado" : "eliminado" } satisfactoriamente
-            </span> */}
-            <div class="warninguser__btn-group">
-              <button onClick={flag === 'N' ? handleCreateUpdate : flag === 'U' ? handleCreateUpdate : handleDelete } className="warninguser__btn">
-                Yes
-              </button>
-              <button className="warninguser__btn">No</button>
-            </div>
-          </form>
+    <div
+      onClick={handleCloseWarning}
+      className={`warninguser-container ${closeWarning && "close-warning"}`}
+    >
+      <form onClick={(e) => e.stopPropagation()} className="warninguser">
+        <i
+          className={
+            flag === "N"
+              ? "fa-solid fa-circle-check warninguser__icon"
+              : flag === "U"
+              ? "fa-solid fa-circle-check warninguser__icon"
+              : "fa-solid fa-circle-question warninguser__icon"
+          }
+        ></i>
+        <h2 className="warninguser__question">
+          {flag === "N"
+            ? "User created successfully"
+            : flag === "U"
+            ? "User updated successfully"
+            : "Are you sure to delete the user?"}
+        </h2>
+        <div onClick={handleCloseWarning} className="warninguser__close">
+          x
+        </div>
+        <div className="warninguser__btn-group">
+          <button
+            onClick={
+              flag === "N"
+                ? handleCreateUpdate
+                : flag === "U"
+                ? handleCreateUpdate
+                : handleDelete
+            }
+            className={`warninguser__btn ${
+              flag === "N" ? "create" : flag === "U" ? "update" : "delete"
+            }`}
+          >
+            {flag === "N" ? "" : flag === "U" ? "" : "Yes"}
+          </button>
+          <button
+            className={`warninguser__btn ${
+              flag === "N" ? "create" : flag === "U" ? "update" : "delete"
+            }`}
+          >
+            {flag === "N" ? "" : flag === "U" ? "" : "No"}
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default WarningUser;
